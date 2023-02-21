83ff6b874481127d70f8fb49114977efa7bb9483
lukang-local <1035525823@email.com>
2023-02-21 23:55:07 +0800

    fix

--- lesson03/src/main/java/com/sohu/controller/SourceController.java
+++ lesson03/src/main/java/com/sohu/controller/SourceController.java
@@ -1,12 +1,12 @@
 package com.sohu.controller;
 
+import com.oppo.tool.ApplicationContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import util.ApplicationContextUtil;
 import util.CustomResourceLoader;
 import util.CustomResourceLoaderByConfig;
 import util.MessageConvertUtils;
