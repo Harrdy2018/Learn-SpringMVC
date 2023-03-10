15ba7fe0f6ab92659aa10f29e01f3a703072d948
lukang-local <1035525823@email.com>
2023-03-11 00:22:39 +0800

    nationnal language

--- lesson03/src/main/java/com/sohu/controller/SourceController.java
+++ lesson03/src/main/java/com/sohu/controller/SourceController.java
@@ -1,6 +1,9 @@
 package com.sohu.controller;
 
+import com.oppo.bean.BaseRespBean;
+import com.oppo.model.NationalLanguage;
 import com.oppo.tool.ApplicationContextUtil;
+import com.oppo.tool.MessageSourceAwareUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,14 +12,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import util.CustomResourceLoader;
 import util.CustomResourceLoaderByConfig;
-import util.MessageConvertUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/v3")
 public class SourceController {
     @Autowired
     @Qualifier(value = "messageUtil")
-    private MessageConvertUtils messageConvertUtils;
+    private MessageSourceAwareUtil messageSourceAwareUtil;
 
     @Autowired
     private CustomResourceLoader customResourceLoader;
@@ -24,11 +29,20 @@ public class SourceController {
     @Autowired
     private CustomResourceLoaderByConfig customResourceLoaderByConfig;
 
+    /**
+     * 国际化测试 http://localhost:8080/lesson03/v3/message?key=123
+     *
+     * @param request request
+     * @param response response
+     * @return resp
+     */
     @RequestMapping(value = "/message", method = RequestMethod.GET)
     @ResponseBody
-    public String display(){
-        messageConvertUtils.readLocaleSpecificMessage();
-        return "success";
+    public BaseRespBean<NationalLanguage> display(HttpServletRequest request, HttpServletResponse response){
+        String key = request.getParameter("key");
+        messageSourceAwareUtil.setCode(key);
+        NationalLanguage nationalLanguage = messageSourceAwareUtil.readLocaleSpecificMessage();
+        return BaseRespBean.success(nationalLanguage);
     }
 
     @RequestMapping(value = "/loadfile", method = RequestMethod.GET)
