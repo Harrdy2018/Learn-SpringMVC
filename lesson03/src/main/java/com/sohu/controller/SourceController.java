c5f69217506a9f1ae5fe5483631cd9d9fa3a1c56
lukang-local <1035525823@email.com>
2023-03-12 23:02:02 +0800

    global Request

--- lesson03/src/main/java/com/sohu/controller/SourceController.java
+++ lesson03/src/main/java/com/sohu/controller/SourceController.java
@@ -4,6 +4,7 @@ import com.oppo.bean.BaseRespBean;
 import com.oppo.model.NationalLanguage;
 import com.oppo.tool.ApplicationContextUtil;
 import com.oppo.tool.MessageSourceAwareUtil;
+import com.oppo.tool.ServletUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import util.CustomResourceLoaderByConfig;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping(value = "/v3")
@@ -63,5 +65,32 @@ public class SourceController {
         System.out.println("context ...");
         CustomResourceLoader tempBean = ApplicationContextUtil.getBean("customResourceLoader", CustomResourceLoader.class);
         return tempBean.showResourceData();
+    }
+
+    /**
+     * 主线程和子线程都能拿到Request对象
+     * http://localhost:8080/lesson03/v3/testServlet?age=18
+     * 可以解决子线程线程中无法获取request对象的问题；但是如果主线程先结束，子线程再去取，会有同样的问题
+     * 解决办法：主线程以参数的形式传给子线程
+     *
+     * @return string
+     */
+    @RequestMapping(value = "/testServlet", method = RequestMethod.GET)
+    @ResponseBody
+    public String display4(){
+        ServletUtils.setRequestObjectShare(true);
+        System.out.println("CurrentThread "+Thread.currentThread().getName() +"URL->"+ ServletUtils.getRequest().getRequestURL());
+        System.out.println("params->"+ ServletUtils.getParameter("age"));
+        new Thread(()->{
+            System.out.println("CurrentThread "+Thread.currentThread().getName());
+            try {
+                Thread.sleep(5000);
+                System.out.println("URL->"+ ServletUtils.getRequest().getRequestURL());
+                System.out.println("params->"+ ServletUtils.getParameter("age"));
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
+        return "OK";
     }
 }
